97f2e8ffe15c08bb44de7c584d58b3c7f350b598
pawo97 <pa-wo97@o2.pl>
2019-05-15 17:48:43 +0200

    gitignoreadded

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
@@ -1,6 +1,9 @@
 package edu.iis.mto.testreactor.exc2;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -10,6 +13,32 @@ public class WashingMachineTest {
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
+    }
+
+    DirtDetector dirtDetector = mock(DirtDetector.class);
+    Engine engine = mock(Engine.class);
+    WaterPump waterPump = mock(WaterPump.class);
+    LaundryBatch laundryBatch;
+    LaundryStatus laundryStatus;
+    Percentage percentage = new Percentage(20);
+    ProgramConfiguration programConfiguration;
+    WashingMachine washingMachine;
+
+    @Test
+    public void Test1() {
+        washingMachine = new WashingMachine(dirtDetector, engine, waterPump);
+
+        programConfiguration = ProgramConfiguration.builder()
+                                                   .withProgram(Program.SHORT)
+                                                   .withSpin(false)
+                                                   .build();
+
+        when(dirtDetector.detectDirtDegree(laundryBatch)).thenReturn(percentage);
+        // when(engine.spin()).thenReturned(false);
+        // when(engine.runWashing(50)).thenReturned()
+        laundryStatus = washingMachine.start(laundryBatch, programConfiguration);
+
+        assertEquals(laundryStatus.getResult(), programConfiguration.getProgram());
     }
 
 }
